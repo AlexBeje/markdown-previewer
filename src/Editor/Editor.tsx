8213a2465d5b2b4c388150561165284387c93914
Alex <alex.bejenaru93@gmail.com>
2020-11-05 08:46:12 +0100

    📦 NEW: Layed out the base structure.

---
+++ src/Editor/Editor.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function editor() {
+  return <div className="w-50">Editor</div>;
+}
---
+++ src/Previewer/Previewer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function previewer() {
+  return <div className="w-50">Previewer</div>;
+}
+
+export default previewer;
